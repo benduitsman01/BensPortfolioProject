9b97745b0946c967f3cbcbea1ee95f20135e5f9c
benduitsman01 <96610451+benduitsman01@users.noreply.github.com>
2022-03-07 17:54:04 -0600

    7-2

--- main.js
+++ main.js
@@ -7,16 +7,60 @@ function scrollHeader(){
 window.addEventListener('scroll', scrollHeader)
 
 /*=============== SERVICES MODAL ===============*/
+const modalViews = document.querySelectorAll('.services__modal'),
+    modalBtns = document.querySelectorAll('.services__button'),
+    modalClose = document.querySelectorAll('.services__modal-close')
 
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((mb, i) =>{
+    mb.addEventListener('click', () =>{
+        modal(i)
+    })
+})
+
+modalClose.forEach((mc) =>{
+    mc.addEventListener('click', () =>{
+        modalViews.forEach((mv) => {
+            mv.classList.remove('active-modal')
+        })
+    })
+})
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
-
+let mixerPortfolio = mixitup('.work__container', {
+    selectors: {
+         target: '.work__card'
+    },
+    animation: {
+         duration: 300
+    }
+});
 
 /* Link active work */ 
+const linkWork = documentQuerySelector('.work__item')
 
+function activeWork(){
+    linkWork.forEach(l=> l.classList.remove('active-work'))
+    this.classList.add('active-work')
+}
+
+linkWork.forEach(l=> l.addEventListener('click', activeWork))
 
 /*=============== SWIPER TESTIMONIAL ===============*/
+let swiperTestimonial = new swiper(".testimonial__container", {
+    spaceBetween: 24,
+    loop: true,
+    grabCursor: true, 
 
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
